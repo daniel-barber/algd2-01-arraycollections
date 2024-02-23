b474de57681d05ea02b0b67cce2dd03df32dccf4
daniel.barber <daniel.barber@students.fhnw.ch>
2024-02-23 22:48:00 +0100

    implement add and contains of UnsortedSet

--- src/main/java/ch/fhnw/algd2/arraycollections/UnsortedSet.java
+++ src/main/java/ch/fhnw/algd2/arraycollections/UnsortedSet.java
@@ -4,52 +4,75 @@ import java.util.Arrays;
 import java.util.Set;
 
 public class UnsortedSet<E> extends AbstractArrayCollection<E> implements Set<E> {
-	public static final int DEFAULT_CAPACITY = 100;
-	private E[] data;
+    public static final int DEFAULT_CAPACITY = 100;
+    private E[] data;
+    private int size;
 
-	public UnsortedSet() {
-		this(DEFAULT_CAPACITY);
-	}
+    public UnsortedSet() {
+        this(DEFAULT_CAPACITY);
+    }
 
-	@SuppressWarnings("unchecked")
-	public UnsortedSet(int capacity) {
-		data = (E[])new Object[capacity];
-	}
+    @SuppressWarnings("unchecked")
+    public UnsortedSet(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
 
-	@Override
-	public boolean add(E e) {
-		// TODO implement unless collection shall be immutable
-		throw new UnsupportedOperationException();
-	}
+    public static void main(String[] args) {
+        UnsortedSet<Integer> bag = new UnsortedSet<Integer>();
+        bag.add(2);
+        bag.add(2);
+        bag.add(1);
+        System.out.println(Arrays.toString(bag.toArray()));
+    }
 
-	@Override
-	public boolean remove(Object o) {
-		// TODO implement unless collection shall be immutable
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean add(E e) {
+        // TODO implement unless collection shall be immutable
+        checkNull(e);
+        if (!contains(e)) {
+            if(size== data.length){
+                throw new IllegalStateException();
+            }
+            data[size++] = e;
+            return true;
+        }
+        return false;
+        //throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean contains(Object o) {
-		// TODO must be implemented
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean remove(Object o) {
+        // TODO implement unless collection shall be immutable
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Object[] toArray() {
-		return Arrays.copyOf(data, size());
-	}
+    @Override
+    public boolean contains(Object o) {
+        // TODO must be implemented
+        checkNull(o);
+        return indexOf(o) != -1;
+        //throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int size() {
-		// TODO must be implemented
-		return 0;
-	}
+    private int indexOf(Object o) {
+        int i = 0;
+        while (i < size && !data[i].equals(o)) {
+            i++;
+        }
+        if (i == size){
+            return -1;
+        }
+        return i;
+    }
 
-	public static void main(String[] args) {
-		UnsortedSet<Integer> bag = new UnsortedSet<Integer>();
-		bag.add(2);
-		bag.add(2);
-		bag.add(1);
-		System.out.println(Arrays.toString(bag.toArray()));
-	}
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(data, size());
+    }
+
+    @Override
+    public int size() {
+        // done must be implemented
+        return size;
+    }
 }
